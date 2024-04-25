a66e233e1777559def343cc4d1ec91de64a8532d
Carissa Ward <cward52@my.bcit.ca>
2024-04-25 13:45:52 -0700

    added controller for testing methods in JWTUtilities

---
+++ Controllers/JwtTestController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OurBeautyReferralNetwork.Utilities;
+
+namespace OurBeautyReferralNetwork.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JwtTestController : ControllerBase
+    {
+        private readonly JWTUtilities _jwtUtilities;
+
+        public JwtTestController(JWTUtilities jwtUtilities)
+        {
+            _jwtUtilities = jwtUtilities;
+        }
+
+        [HttpGet("GenerateRandomKey/{keyLength}")]
+        [AllowAnonymous]
+        public IActionResult GenerateRandomKey(int keyLength)
+        {
+            var randomKey = JWTUtilities.GenerateRandomKey(keyLength);
+            return Ok(new { RandomKey = randomKey });
+        }
+
+        [HttpGet("GenerateJwtToken")]
+        [AllowAnonymous]
+        public IActionResult GenerateJwtToken(string email)
+        {
+            var token = _jwtUtilities.GenerateJwtToken(email);
+            return Ok(new { Token = token });
+        }
+    }
+}
