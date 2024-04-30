b7b9fe67292d2dc131f791f1a4cbdd86349e24c0
Amanda Mar <msamandamar@gmail.com>
2024-04-29 20:46:34 -0700

    discount debugging

---
+++ EntityExtensions/DiscountDTO.cs
@@ -0,0 +1,12 @@
+﻿using OurBeautyReferralNetwork.Models;
+
+namespace OurBeautyReferralNetwork.EntityExtensions
+{
+    internal class DiscountDTO : Discount
+    {
+        public string PkDiscountId { get; set; }
+        public decimal Percentage { get; set; }
+        public DateOnly? StartDate { get; set; }
+        public DateOnly? EndDate { get; set; }
+    }
+}
